265d8ac69d563acc0b962cb64f49572473b6fd52
Barbato Federico <45495935+Developer310301@users.noreply.github.com>
2023-08-07 16:57:33 +0200

    feat: move from pointers to shared_ptr

--- include/smartgraph/interface/IGraph.h
+++ include/smartgraph/interface/IGraph.h
@@ -1,8 +1,13 @@
+#ifndef SMARTGRAPH_IGRAPH_H
+#define SMARTGRAPH_IGRAPH_H
+
 #include "smartgraph/interface/Vertex.h"
 #include "smartgraph/interface/Edge.h"
 
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
+#include <memory>
 
 namespace smartgraph::interface{
 
@@ -14,17 +19,19 @@ namespace smartgraph::interface{
      */
     template<typename V, typename E>
     class IGraph{
-
-        using AdjancencyMap = std::unordered_map<Vertex<V>,std::unordered_map<Vertex<V>*,Edge<E>*>>;
-        using EdgeList = std::vector<Edge<E>>;
+        using VertexPointer = std::shared_ptr<Vertex<V>>;
+        using EdgePointer = std::shared_ptr<Edge<E>>;
+        using AdjancencyMap = std::unordered_map<Vertex<V>,std::unordered_map<VertexPointer,EdgePointer>>;
+        using EdgeList = std::unordered_set<Edge<E>>;
 
         private:
-            AdjancencyMap _adjacencyMap;  //Map of vetexes andacencies.
-            EdgeList _edges;                //List of edges.
+            AdjancencyMap _adjacencyMap{};  //Map of vetexes andacencies.
+            EdgeList _edges{};                //List of edges.
 
         public:
 
-            IGraph() : _adjacencyMap(AdjancencyMap(10)), _edges(EdgeList(10)){}
+            //IGraph() : _adjacencyMap(AdjancencyMap(10)), _edges(EdgeList(10)){}
+            IGraph(){};
 
             /**
              * @brief Get the number of vertex in the graph.
@@ -38,7 +45,7 @@ namespace smartgraph::interface{
              * 
              * @return std::vector<Vertex<V>> vector of vertex.
              */
-            virtual std::vector<Vertex<V>*> vertices() = 0;
+            virtual std::vector<VertexPointer> vertices() = 0;
 
             /**
              * @brief Get the number of edges in the graph.
@@ -52,7 +59,7 @@ namespace smartgraph::interface{
              * 
              * @return std::vector<Edge<E>*> vector of edges.
              */
-            virtual std::vector<Edge<E>*> edges() = 0;
+            virtual std::vector<EdgePointer> edges() = 0;
 
             /**
              * @brief Get the Edge reference object between the edges ``u`` and ``v``.
@@ -61,7 +68,7 @@ namespace smartgraph::interface{
              * @param v ending vertex.
              * @return Edge<E>* reference to the edge.
              */
-            virtual Edge<E>* getEdge(Vertex<V>* u, Vertex<V>* v) = 0;
+            virtual EdgePointer getEdge(VertexPointer u, VertexPointer v) = 0;
 
             /**
              * @brief Get the two vertices of the specified edge.
@@ -69,7 +76,7 @@ namespace smartgraph::interface{
              * @param e Edge used to get the two vertexes.
              * @return std::vector<Vertex<V>*> Array containing the references to the two vertexes of the given Edge.
              */
-            virtual std::vector<Vertex<V>*> endVertices(Edge<E>* e) = 0;
+            virtual std::vector<VertexPointer> endVertices(Edge<E>* e) = 0;
 
             /**
              * @brief Get the terminal vertex of ``e`` different from ``v``.
@@ -78,7 +85,7 @@ namespace smartgraph::interface{
              * @param e Edge where to look for.
              * @return Vertex<V>* The opposite vertex of ``v``, ``nullptr`` if ``e`` isn't incident with ``v``.
              */
-            virtual Vertex<V>* opposite(Vertex<V>* v, Edge<E>* e) = 0;
+            virtual VertexPointer opposite(VertexPointer v, EdgePointer e) = 0;
 
             /**
              * @brief Returns the number of outgoing vertexes from vertex ``v``.
@@ -86,7 +93,7 @@ namespace smartgraph::interface{
              * @param v Vertex which getting the outgoing vertexes.
              * @return unsigned long Number of outgoing vertexes.
              */
-            virtual unsigned long outDegree(Vertex<V>* v) = 0;
+            virtual unsigned long outDegree(VertexPointer v) = 0;
 
             /**
              * @brief Returns the number of incoming vertexes to vertex ``v``.
@@ -94,7 +101,7 @@ namespace smartgraph::interface{
              * @param v Vertex which getting the incoming vertexes.
              * @return unsigned long Number of incoming vertexes.
              */
-            virtual unsigned long inDegree(Vertex<V>* v) = 0;
+            virtual unsigned long inDegree(VertexPointer v) = 0;
 
             /**
              * @brief Returns an array of incoming Edges from vertex ``v``.
@@ -102,7 +109,7 @@ namespace smartgraph::interface{
              * @param v Vertex which getting the incoming edges.
              * @return std::vector<Edge<E>*> Array of incoming edges.
              */
-            virtual std::vector<Edge<E>*> outgoingEdges(Vertex<V>* v) = 0;
+            virtual std::vector<EdgePointer> outgoingEdges(VertexPointer v) = 0;
 
             /**
              * @brief Returns an array of incoming Edges to vertex ``v``.
@@ -110,7 +117,7 @@ namespace smartgraph::interface{
              * @param v Vertex which getting the incoming edges.
              * @return std::vector<Edge<E>*> Array of incoming edges.
              */
-            virtual std::vector<Edge<E>*> incomingEdges(Vertex<V>* v) = 0;
+            virtual std::vector<EdgePointer> incomingEdges(VertexPointer v) = 0;
 
             /**
              * @brief Insert a new @see Vertex that will contains an object ``x`` and then it will return a reference to the new vertex.
@@ -118,7 +125,7 @@ namespace smartgraph::interface{
              * @param x Content of a new vertex.
              * @return Vertex<V>* Reference to the created vertex.
              */
-            virtual Vertex<V>* insertVertex(V x) = 0;
+            virtual VertexPointer insertVertex(V x) = 0;
 
             /**
              * @brief Insert a new @see Edge that will contains an object ``y`` between the vertexes ``u`` and ``v`` and it will return a reference to the new edge.
@@ -128,7 +135,7 @@ namespace smartgraph::interface{
              * @param x Content of the new edge.
              * @return Edge<E>* Reference to the new edge.
              */
-            virtual Edge<E>* insertEdge(Vertex<V>* u, Vertex<V>* y, E x) = 0;
+            virtual EdgePointer insertEdge(VertexPointer u, VertexPointer y, E x) = 0;
 
             /**
              * @brief Remove the vertex from the graph and all the incident edges from him
@@ -136,7 +143,7 @@ namespace smartgraph::interface{
              * @param v Vertex to remove
              * @return int returns 1 if correctly removed, 0 if the vertex doesn't exist in the graph, -1 for others error
              */
-            virtual int removeVertex(Vertex<V>* v) = 0;
+            virtual int removeVertex(VertexPointer v) = 0;
 
             /**
              * @brief Remove the edge from the graph
@@ -144,8 +151,10 @@ namespace smartgraph::interface{
              * @param e Edge to remove
              * @return int returns 1 if correctly removed, 0 if the edge doesn't exist in the graph, -1 for others error
              */
-            virtual int removeEdge(Edge<E>* e) = 0;
+            virtual int removeEdge(EdgePointer e) = 0;
 
     };
 
 }
+
+#endif
